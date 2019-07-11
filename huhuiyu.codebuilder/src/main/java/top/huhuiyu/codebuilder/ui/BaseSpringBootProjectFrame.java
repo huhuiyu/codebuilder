13b2ade0de624f6cab683bb4fe1fbb1d31ce8b3c
huhuiyu <1069306849@qq.com>
2019-07-11 16:04:03 +0800

    删除默认信息

--- huhuiyu.codebuilder/src/main/java/top/huhuiyu/codebuilder/ui/BaseSpringBootProjectFrame.java
+++ huhuiyu.codebuilder/src/main/java/top/huhuiyu/codebuilder/ui/BaseSpringBootProjectFrame.java
@@ -96,23 +96,7 @@ public abstract class BaseSpringBootProjectFrame extends JFrame implements Actio
   }
 
   private void setTestDefault() {
-    // 测试用默认值
-    tfProjectName.setText("OnlineExamService");
-    tfPackageName.setText("top.huhuiyu.onlineexam");
-    tfAuthor.setText("胡辉煜");
-    tfIp.setText("39.108.124.237");
-    tfPort.setText("3306");
-    tfUsername.setText("test-projects");
-    tfPassword.setText("Test4-sql");
-    tfDatabase.setText("online_exam");
-    // tfProjectName.setText("CDXP1688DataService");
-    // tfPackageName.setText("com.cdxp1688.dataservice");
-    // tfAuthor.setText("胡辉煜");
-    // tfIp.setText("39.108.124.237");
-    // tfPort.setText("3306");
-    // tfUsername.setText("test-projects");
-    // tfPassword.setText("Test4-sql");
-    // tfDatabase.setText("CDXP1688");
+    
 
     tfOutputDir.setText("d:\\output");
   }
